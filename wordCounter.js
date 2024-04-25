b03c710a32369f9bb8ba4da41e7a7811849209c6
muhammadmubashir72 <mubashirkhi72@gmail.com>
2024-04-26 03:22:25 +0500

    i have a created yummy cli word counter

---
+++ wordCounter.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green.bold(`\n\t Welcome to the CLI Word Counter \n`));
+console.log(chalk.cyan.bold("\t ^_^_^_^_^_^_^_^_^_^_^_^_^_^_^_^\n"));
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.yellow("Enter your Sentence:")
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.blueBright("The words in your sentence are " + chalk.red.bold(words.length)));
